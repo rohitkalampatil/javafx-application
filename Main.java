a6e919474480d0430e8123406a4c3609b3999a13
rohitkalampatil <rohitkalampatil@gmail.com>
2023-08-29 21:03:13 +0530

    added event listener on button

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Main extends Application implements EventHandler<ActionEvent> {
 
     Button button;
 
@@ -15,29 +14,42 @@ public class Main extends Application {
         launch(args);
     }
 
-    public void start (Stage primaryStage) throws Exception{
-        //Added title to main window
-        
+    public void start(Stage primaryStage) throws Exception {
+        // Added title to main window
+
         primaryStage.setTitle("Main page");
-        
-        //created button
-        
+
+        // created button
+
         button = new Button();
+
+        // sets text on button
         button.setText("click");
-        
+
+        // add event on button i.e to make button responsive whenever user click
+        // it will need to implement eventhandler
+        button.setOnAction(this);
+        // this stand for event is gona handle in this class
+
         // to display any thing on window we need create layout
-        //StackPane creates a layout and we need to add children i.e element on layout
-        
+        // StackPane creates a layout and we need to add children i.e element on layout
+
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
-        
-        //now we need to create scene to display our button or children
-        //scene is content on your window
 
-        Scene scene =new Scene(layout, 300, 250);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        // now we need to create scene to display our button or children
+        // scene is content on your window
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
-        
+
+    // to add ebvent handler when user click button
+    public void handle(ActionEvent event) {
+        if (event.getSource() == button) {
+            System.out.println("touched button");
+        }
+    }
 }
