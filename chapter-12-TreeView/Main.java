543345abe774d6e06be7a5d5b3461f6baccd0bdf
rohitkalampatil <rohitkalampatil@gmail.com>
2023-09-02 12:03:26 +0530

    added event Listener to TreeView

--- chapter-12-TreeView/Main.java
+++ chapter-12-TreeView/Main.java
@@ -1,5 +1,7 @@
 import com.sun.crypto.provider.HmacMD5KeyGenerator;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -46,6 +48,14 @@ public class Main extends Application {
         //create treeView
         treeView = new TreeView<>(root);
         treeView.setShowRoot(false);
+        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>() {
+            @Override
+            public void changed(ObservableValue<? extends TreeItem<String>> observable, TreeItem<String> oldValue, TreeItem<String> newValue) {
+                if (newValue!= null)
+                    System.out.println(newValue.getValue());
+            }
+        });
+
         // Layout
         StackPane layout = new StackPane();
         layout.getChildren().addAll(treeView);
@@ -56,7 +66,7 @@ public class Main extends Application {
     }
 
     public TreeItem<String> makeBranch(String title,TreeItem<String> parent){
-        TreeItem<String > item = new TreeItem<>(title);
+        TreeItem<String> item = new TreeItem<>(title);
         item.setExpanded(false);
         parent.getChildren().add(item);
         return item;
