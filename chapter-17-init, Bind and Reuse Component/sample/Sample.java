67ff265a9474fba5af08f1f8c7c11659203b2be4
rohitkalampatil <rohitkalampatil@gmail.com>
2023-09-03 10:55:30 +0530

    chapter-17-initialize,Bind,reuse components

---
+++ chapter-17-init, Bind and Reuse Component/sample/Sample.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Sample implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("loading data");
+    }
+}
